790eba953a7b026c957db9c275cb4cf0a2c74054
walid elzo <walidelzo@walidelzo-macbook-pro.local>
2018-06-20 19:03:16 +0200

    Initial Commit

---
+++ Pirate Adventure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pirate Adventure
+//
+//  Created by walidelzo on 6/20/18.
+//  Copyright © 2018 walidelzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pirate Adventure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pirate Adventure
+//
+//  Created by walidelzo on 6/20/18.
+//  Copyright © 2018 walidelzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
